e82f3824965adefa5805fdc12dbfff99dcbf056d
Aswanth Updot <aswanth@updot.co>
2026-04-05 06:25:22 -0700

    fix: test

--- src/components/icons/chevron-left.tsx
+++ src/components/icons/chevron-left.tsx
@@ -7,6 +7,7 @@ const IconChevronLeft = ({ className }: { className?: string }) => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className={className}
+      
     >
       <path
         d="M4.33966 5.62011L-8.7071e-07 9.95976L1.28048 11.2402L6.90059 5.62011L1.28048 1.11943e-07L-1.11942e-07 1.28047L4.33966 5.62011Z"
